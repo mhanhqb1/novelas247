68da9ddf6bdee143aa8b0369574e7ad84fc2f377
AnhMH <anh.mh@moziasoft.com>
2020-12-02 14:14:52 +0700

    Add function clear cache

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,50 @@ Route::post("/{$routeAdmin}/movie/save", [AdminController::class, 'movieSave'])-
 Route::get("/{$routeAdmin}/video/add/{movieId}", [AdminController::class, 'videoAdd'])->name('admin.video_add');
 Route::post("/{$routeAdmin}/video/save", [AdminController::class, 'videoSave'])->name('admin.video_save');
 Route::get("/{$routeAdmin}/video/edit/{videoId}", [AdminController::class, 'videoEdit'])->name('admin.video_edit');
+
+
+//Clear Cache facade value:
+Route::get('/s-clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/s-migrate', function() {
+    $exitCode = Artisan::call('migrate');
+    return '<h1>migrate</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/s-optimize', function() {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/s-route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/s-route-clear', function() {
+    $exitCode = Artisan::call('route:clear');
+    return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/s-view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/s-config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    $exitCode = Artisan::call('view:clear');
+    $exitCode = Artisan::call('route:clear');
+    $exitCode = Artisan::call('route:cache');
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Clear Config cleared</h1>';
+});
