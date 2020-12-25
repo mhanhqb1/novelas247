355f3f0b0dff74ba690ec93503eb1dad7e896177
AnhMH <anh.mh@moziasoft.com>
2020-12-25 22:15:35 +0700

    Add missing file

---
+++ resources/views/layout/video/video_ads.blade.php
@@ -0,0 +1,11 @@
+<script>
+          window["596511tytnbd530458jzeiny"] = {
+            zoneId: 1807355,
+            domain: "//rozamimo9za10.com",
+            options: {
+              insteadOfSelectors: ["#my-video-player"],
+              insteadOfPlayers: ["other"]
+            }
+          }
+        </script>
+        <script src="https://rozamimo9za10.com/zbs.kek.js"></script>
